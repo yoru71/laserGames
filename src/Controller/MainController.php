b7e164035211da9685b84ba90f3b224f93f318b9
yoru <lancinotvalentin71@gmail.com>
2020-01-21 16:00:37 +0100

    All Teamplates

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -8,12 +8,75 @@ use Symfony\Component\Routing\Annotation\Route;
 class MainController extends AbstractController
 {
     /**
-     * @Route("", name="acceuil")
+     * @Route("", name="accueil")
      */
-    public function acceuil()
+    public function accueil()
     {
-        return $this->render('main/acceuil.html.twig', [
+        return $this->render('main/accueil.html.twig', [
             'controller_name' => 'MainController',
         ]);
     }
+    /**
+     * @Route("autunMenu", name="autunMenu")
+     */
+    public function autunMenu()
+    {
+        return $this->render('main/autunMenu.html.twig');
+    }
+    /**
+     * @Route("chamonixMenu", name="chamonixMenu")
+     */
+    public function chamonyMenu()
+    {
+        return $this->render('main/chamonixMenu.html.twig');
+    }
+    /**
+     * @Route("laserGameAutun", name="laserGameAutun")
+     */
+    public function laserGameAutun()
+    {
+        return $this->render('main/laserGameAutun.html.twig');
+    }
+    /**
+     * @Route("laserGameChamonix", name="laserGameChamonix")
+     */
+    public function laserGameChamonix()
+    {
+        return $this->render('main/laserGameChamonix.html.twig');
+    }
+    /**
+     * @Route("salleGaming", name="salleGaming")
+     */
+    public function salleGaming()
+    {
+        return $this->render('main/salleGaming.html.twig');
+    }
+    /**
+     * @Route("lancerHacheAutun", name="lancerHacheAutun")
+     */
+    public function lancerHacheAutun()
+    {
+        return $this->render('main/lancerHacheAutun.html.twig');
+    }
+    /**
+     * @Route("lancerHacheChamonix", name="lancerHacheChamonix")
+     */
+    public function lancerHacheChamonix()
+    {
+        return $this->render('main/lancerHacheChamonix.html.twig');
+    }
+    /**
+     * @Route("bubbleFootAutun", name="bubbleFootAutun")
+     */
+    public function bubbleFootAutun()
+    {
+        return $this->render('main/bubbleFootAutun.html.twig');
+    }
+    /**
+     * @Route("bubbleFootChamonix", name="bubbleFootChamonix")
+     */
+    public function bubbleFootChamonix()
+    {
+        return $this->render('main/bubbleFootChamonix.html.twig');
+    }  
 }
